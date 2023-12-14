58eea355a228674b8013de355dcc83de65f4afa4
Lewis Hardcastle <lewishardcastle@proton.me>
2023-12-14 15:22:59 +0000

    dark-mode2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './AppDarkMode.css';
 import App from './App';
 import App2 from './App2';
 import App3 from './App3';
